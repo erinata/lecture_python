14a3a44aa2e8cb1e1b46585a18fe32a0e3122b2e
Chungsang Tom Lam <erinata@gmail.com>
2019-01-22 16:20:26 -0500

    testing python numbers

---
+++ python_test.py
@@ -0,0 +1,75 @@
+print("hello")
+
+print(3+3)
+
+print(3*3)
+
+print(19/3)
+
+print(19//3)
+
+print(19%3)
+
+print(18/3)
+
+print(5**2)
+
+print(2**10)
+
+a = 10
+b = 20
+print(a+b)
+
+print("With Variable")
+a = 31
+# If you set the variable at one place, 
+# you do not need to change it in multiple places when you need to do refactoring.
+print(a/3)
+print(a//3)
+print(a%3)
+print("\n")
+
+# print(c+1)
+
+print("hello world")
+
+def addandmultiply(a,b):
+	print(a+b)
+	print(a*b)
+
+addandmultiply(10,20)
+addandmultiply(2,3)
+addandmultiply(5,100)
+
+################
+# In R
+# addandmultiply <- function(a,b) {
+# 	print(a+b)
+# 	print(a*b)
+# }
+
+print(round(3.141592654,3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
