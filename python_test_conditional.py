2fdec2c7f67e234b38d5b6e77fffd99a366a4cf1
Chungsang Tom Lam <erinata@gmail.com>
2019-01-22 23:44:11 -0500

    flow control

---
+++ python_test_conditional.py
@@ -0,0 +1,54 @@
+x = 5
+
+if x < 0:
+	print("X is smaller than 0.")
+elif x > 10:
+	print("X is bigger than 10.")
+else:
+	print("X is between 0 and 10.")
+
+
+########
+#y = int(input("enter value of y:"))
+
+y=10
+if y < 0:
+	print("y is small.")
+elif y >= 0:
+	print("y is big.")
+else:
+	print("error")
+
+######
+
+animals  = ['cat','dog', 'turtle','bird']
+
+print(animals)
+
+for x in animals:
+	print(x)
+	print(len(x))
+
+#numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = range(10)
+
+for i in numbers:
+	print(i**2)
+
+def complicated_function():
+	pass
+
+### In R
+# complicated_function <- function() {
+	
+# }
+
+
+
+
+
+
+
+
+
+
